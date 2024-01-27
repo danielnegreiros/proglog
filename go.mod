e0bd868ed98afa6ea8d4371ecab434efe1ab4ad3
Daniel Barros <daniel@proxmox.zebreje.uk>
2024-01-27 12:09:29 -0300

    studying

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danielnegreiros/proglog
+
+go 1.21.6
---
+++ internal/server/log.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	ErrOffsetNotFound = fmt.Errorf("offset not found")
+)
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+type Log struct {
+	records []Record
+	mu      sync.Mutex
+}
+
+func (l *Log) Append(record Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	size := len(l.records)
+	record.Offset = uint64(size)
+	l.records = append(l.records, record)
+	return uint64(size), nil
+}
+
+func (l *Log) Read(offset uint64) (Record, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if offset >= uint64(len(l.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+
+	return l.records[offset], nil
+}
